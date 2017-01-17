fcb84f4d6834b95ef75050ba500a45f0a3042b77
Oleg Legun <oleg.legun@gmail.com>
2017-01-17 19:51:11 +0300

    Add Record() for comments

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,8 +1,15 @@
 import {  } from '../constants'
 import { normalizedComments } from '../fixtures'
 import { arrayToMap } from '../helpers'
+import { Record } from 'immutable'
 
-const defaultState = arrayToMap(normalizedComments)
+const commentModel = Record({
+    id: null,
+    user: null,
+    text: null
+})
+
+const defaultState = arrayToMap(normalizedComments, commentModel)
 
 export default (state = defaultState, action) => {
     const { type, payload, response, error } = action
